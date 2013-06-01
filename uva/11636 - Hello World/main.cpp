398d0731179e3736567f0fe7a532bec5f491b8f9
ismdeep <izumu@ism.name>
2013-06-01 14:38:21 +0800

    uva 11636

--- uva/11636 - Hello World/main.cpp
+++ uva/11636 - Hello World/main.cpp
@@ -1,27 +1,39 @@
+/*
+uva 11636
+PROB:  Hello World!
+*/
+
+
 #include <iostream>
 using namespace std;
 
 int cal_ans (int value)
 {
-	int count_times = 0;
+	int __count__ = 0;
 	int tmp = 1;
-	while (tmp < value)
+	while (tmp * 2 < value)
 	{
-		count_times++;
+		__count__++;
 		tmp *= 2;
 	}
-	return count_times;
+	if (tmp != value)
+	{
+		__count__++;
+	}
+	return __count__;
 }
-
 
 int main ()
 {
+	int value;
 	int case_index = 0;
-	int n;
-	while (cin >> n, -1 != n)
+	while (cin >> value && value > 0)
 	{
-		cout << "Case " << ++ case_index << ": " << cal_ans (n) << endl;
+		case_index++;
+		cout << "Case " << case_index << ": ";
+		cout << cal_ans (value) << endl;
 	}
+	return 0;
 }
 
 
