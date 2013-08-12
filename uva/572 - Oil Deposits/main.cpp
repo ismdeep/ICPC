c5bab5dde0a5969ab47d5c0d5928f77254e8a291
ismdeep <izumu@.ism.name>
2013-08-12 14:57:45 +0800

    136 rename，572 oil desposits 575  skew binary

---
+++ uva/572 - Oil Deposits/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_MAP_SIZE 200
+
+int dir[8][2] = {
+	{-1, -1},
+	{-1,  0},
+	{-1,  1},
+	{ 1, -1},
+	{ 1,  0},
+	{ 1,  1},
+	{ 0, -1},
+	{ 0,  1},
+};
+
+
+int n, m;
+
+bool oil_map[MAX_MAP_SIZE][];
+
+
+int main ()
+{
+	while (cin >> n >> m && n + m)
+	{
+	}
+	return 0;
+}
+
+
